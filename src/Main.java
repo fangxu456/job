fa2ffae27ff3eda17b80f353dad13865dc849114
YJSYF <923371677@qq.com>
2016-09-08 16:59:03 +0800

    practice

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,21 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-        while(in.hasNext()){
-            String s = in.nextLine();
-            System.out.println(getResult(s));
-        }
-    }
-    public  static int getResult(String s){
-        int len =s.length();
-        String res = new StringBuffer(s).reverse().toString();
-        int[][] data = new int[len+1][len+1];
-        for(int i=0;i<len+1;i++){
-            data[i][0]=data[0][i]=0;
-        }
-        for(int i=1;i<len+1;i++){
-            for(int j=1;j<len+1;j++){
-                if(s.charAt(i-1)==res.charAt(j-1)){
-                    data[i][j]=data[i-1][j-1]+1;
-                }else if(data[i-1][j]>=data[i][j-1]){
-                    data[i][j]=data[i-1][j];
-                }else
-                    data[i][j]=data[i][j-1];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //Main m = new Main();
+        while(sc.hasNext()) {
+            int num = sc.nextInt();
+            for(int i=0; i<num; i++) {
+                System.out.println(Fan(sc.nextInt()));
             }
         }
 
-        return len-data[len][len];
+    }
+    public static int Fan(int n) {
+        if(n == 1) return 0;
+        if(n == 2) return 1;
+        if(n == 3) return 2;
+        return Fan(n-1)+Fan(n-2);
     }
 }
---
+++ src/saima/Jgch.java
@@ -0,0 +1,33 @@
+package saima;
+
+import java.util.Scanner;
+
+/**
+ * Created by YJSYF on 2016/9/8.
+ * 击鼓传花
+ * 动态规划
+ */
+import java.util.Scanner;
+
+public class Jgch {
+    public static void main(String[] args) {
+        int dp[][] = new int [35][35];
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int m = scan.nextInt();
+        dp[0][1]=1;
+        dp[1][n]=1;
+        dp[1][2]=1;
+        for(int i=1;i <= m;i++) {
+            for(int j=1;j <= n;j++) {
+                if(j == 1)
+                    dp[i][j]=dp[i-1][n]+dp[i-1][2];
+                else if(j==n)
+                    dp[i][j]=dp[i-1][1]+dp[i-1][n-1];
+                else
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j+1];
+            }
+        }
+        System.out.println(dp[m][1]);
+    }
+}
