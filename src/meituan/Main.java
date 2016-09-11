7dca9fcb842a9372a74fb5514a9251a0a7f00ff3
FangXu <923371677@qq.com>
2016-09-12 01:12:13 +0800

    laptop

--- src/meituan/Main.java
+++ src/meituan/Main.java
@@ -1,6 +1,8 @@
 package meituan;
 
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.StringJoiner;
 
 /**
  * Created by YJSYF on 2016/9/11.
@@ -8,9 +10,34 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
-
-
-
+        int N = in.nextInt();
+        int[] a = new int[N];
+        in.nextLine();
+        //for(int j=0; j<N; j++) {
+        int j = 0;
+        while (in.hasNext()){
+            String str = in.nextLine();
+            String[] numList = str.split(",");
+            int[] temp = new int[numList.length];
+            for (int i = 0; i < temp.length; i++) {
+                temp[i] = Integer.parseInt(numList[i]);
+                System.out.println(temp[i]);
+            }
+            a[j]= getMax(temp);
+            j++;
+        }
     }
+    public static int getMax(int[] temp){
+        int[] begin = temp;
+        int sum = 0;
+        Arrays.sort(temp);
+        for(int i=temp.length-1;i>=0;i--){
+            /*if(isOk(temp[i])){
+                sum+=temp[i];
+            }*/
+        }
+        return 1;
+    }
+
+
 }
