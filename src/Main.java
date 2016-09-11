1692a0fa7326db958a482a7a0dd300deb9f97e81
YJSYF <923371677@qq.com>
2016-09-11 17:35:14 +0800

    practice

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 /**
  * Created by YJSYF on 2016/9/11.
  */
 public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
+
+
+
+    }
 }
--- src/meituan/Main.java
+++ src/meituan/Main.java
@@ -1,8 +1,16 @@
 package meituan;
 
+import java.util.Scanner;
+
 /**
  * Created by YJSYF on 2016/9/11.
  */
 public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
+
+
+
+    }
 }
