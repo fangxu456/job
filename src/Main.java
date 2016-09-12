db82c2c652ab0ca77ad0cab706482de5a1d6da8d
YJSYF <923371677@qq.com>
2016-09-12 22:35:24 +0800

    practice

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        System.out.println(countNum(n));
-
-    }
-    public static long countNum(int n){
-        long res =1;
-        if(n==1){
-            return 3;
-        }else{
-            for(int i=1;i<=n;i++){
-                res = res*3;
-            }
-            return res-(n-2)*6;
-        }
 
     }
 }
