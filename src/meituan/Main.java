4936bcaddc0a4beaba38eda7ec3a38997692b29a
YJSYF <923371677@qq.com>
2016-09-12 16:02:24 +0800

    practice

--- src/meituan/Main.java
+++ src/meituan/Main.java
@@ -3,41 +3,45 @@ package meituan;
 import java.util.HashMap;
 import java.util.Arrays;
 import java.util.Scanner;
-import java.util.StringJoiner;
 
 /**
  * Created by YJSYF on 2016/9/11.
+ * 红包问题，动态规划
  */
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        HashMap<String, String> hashMap = new HashMap<>();
+        /*HashMap<String, String> hashMap = new HashMap<>();
         while(in.hasNextLine()){
             String str = in.nextLine();
             hashMap.put(str.substring(0,str.indexOf(",")),str);
-            //System.out.println(hashMap.get("1"));
-        }
+        }*/
         int N = in.nextInt();
         int[] a = new int[N];
+        //这一步很重要，将光标移到下一行
         in.nextLine();
-        //for(int j=0; j<N; j++) {
-        int j = 0;
-        while (in.hasNext()){
+        for(int j = 0; j<N; j++){
             String str = in.nextLine();
             String[] numList = str.split(",");
             int[] temp = new int[numList.length];
             for (int i = 0; i < temp.length; i++) {
                 temp[i] = Integer.parseInt(numList[i]);
-                System.out.println(temp[i]);
+                //System.out.println(temp[i]);
             }
-            a[j]= getMax(temp);
-            j++;
+            //第一类，选取红包0，然后对红包2到红包n-2进行DP
+            int ans = temp[0] + Choose(2, temp.length - 2,temp);
+            //第二类，不选取红包0，然后对红包1到红包n-1进行DP
+            ans = Math.max(ans, Choose(1, temp.length - 1,temp));
+            a[j]= ans;
+        }
+        for(int k : a){
+            System.out.println(k);
         }
     }
     public static int getMax(int[] temp){
-        int[] begin = temp;
+        /*int[] begin = temp;
         int sum = 0;
-        Arrays.sort(temp);
+        Arrays.sort(temp);*/
         for(int i=temp.length-1;i>=0;i--){
             /*if(isOk(temp[i])){
                 sum+=temp[i];
@@ -45,11 +49,20 @@ public class Main {
         }
         return 1;
     }
-
-        System.out.println(getNum(hashMap));
+    public static int Choose(int begin, int end, int[] temp){
+        if (end < begin) return 0;
+        //dp[0]；不选取当前位置红包 dp[1]: 选取当前位置红包
+        int[]  dp = new int[2];
+        int cache;
+        for (int i = begin; i <= end; i++)
+        {
+            //保存dp[0]
+            cache = dp[0];
+            //更新dp[0]:上一轮中的最大值
+            dp[0] = Math.max(dp[0], dp[1]);
+            //更新dp[1]: 当前红包值+上一轮dp[0]
+            dp[1] = cache + temp[i];
+        }
+        return Math.max(dp[0], dp[1]);
     }
-    public static int getNum(HashMap hashMap){
-        return 0;
-    }
-
 }
