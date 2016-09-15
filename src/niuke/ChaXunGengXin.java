ded119e6c7b1f1506b50599970efd6746186dc9a
YJSYF <923371677@qq.com>
2016-09-15 12:58:45 +0800

    practice

--- src/niuke/ChaXunGengXin.java
+++ src/niuke/ChaXunGengXin.java
@@ -20,7 +20,9 @@ public class ChaXunGengXin {
             String[] list = str.split(" ");
             if(list[0].equals("Q")){
                 int temp=0;
-                for(int k=Integer.parseInt(list[1]);k<=Integer.parseInt(list[2]);k++){
+                int m = Math.max(Integer.parseInt(list[1]),Integer.parseInt(list[2]));
+                int n = Math.min(Integer.parseInt(list[1]),Integer.parseInt(list[2]));
+                for(int k=n;k<=m;k++){
                     if(a[k-1]>temp){
                         temp=a[k-1];
                     }
