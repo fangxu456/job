64be99af26d39d34d2435eee6a3005056f49a9f7
FangXu <923371677@qq.com>
2016-09-20 01:00:33 +0800

    laptop

---
+++ src/Test.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class FirstRepeat {
+    public char findFirstRepeat(String A, int n) {
+        List<Character> hm= new ArrayList<Character>();
+        char[] c = A.toCharArray();
+        char flag = ' ' ;
+        for(int i=0;i<n;i++){
+            if(!hm.contains(c[i])){
+                hm.add(c[i]);
+            }else{
+                flag = c[i];
+                break;
+            }
+        }
+        return flag;
+
+    }
+}
---
+++ src/baidu/Main.java
@@ -0,0 +1,7 @@
+package baidu;
+
+/**
+ * Created by FangXu on 2016/9/20.
+ */
+public class Main {
+}
