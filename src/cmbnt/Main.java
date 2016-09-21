25f56267f232099c579286f56086188e370e295e
FangXu <923371677@qq.com>
2016-09-21 21:15:45 +0800

    laptop

--- src/cmbnt/Main.java
+++ src/cmbnt/Main.java
@@ -1,7 +1,13 @@
 package cmbnt;
 
+import java.io.File;
+
 /**
  * Created by YJSYF on 2016/9/18.
  */
 public class Main {
+    public static void main(String[] args){
+        File file = new File("name");
+        File[] files = file.listFiles();
+    }
 }
